c2f29b83a4afbbf0ef3ddf5d05a3c01dfe37e15a
Gabriel Vasconcelos <gabriel.souza@rbmweb.com.br>
2022-06-03 14:03:07 -0300

    Finalizacao

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const Edit = document.querySelectorAll('.input-dados');
-
+const button = document.querySelector('#btn-login');
 //Pegar o evento de focar nos Edit e adicionar a classe active na Label
 Edit.forEach((e)=>{
     e.addEventListener('focus',({target})=>{
@@ -10,6 +10,20 @@ Edit.forEach((e)=>{
       const span = target.previousElementSibling;
       if(target.value.length === 0){
         span.classList.remove('active');
+      }
+    })
+    e.addEventListener('blur', ()=>{
+      let disabled = false;
+      Edit.forEach((e)=>{
+        if(e.value.length === 0){
+          disabled = true;
+        }
+        
+      })
+      if(!disabled){
+          button.removeAttribute('disabled');
+      } else {
+        button.setAttribute('disabled', '');
       }
     })
 })
